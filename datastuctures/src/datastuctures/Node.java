3f752caeee517b079c4eaa580bdf1df5c26bf6cb
Harsha <Harshagowda3817@gmail.com>
2020-12-23 21:29:23 +0530

    Simple Linked list code in JAVA

---
+++ datastuctures/src/datastuctures/Node.java
@@ -0,0 +1,6 @@
+package datastuctures;
+
+public class Node {
+ int data;
+ Node next;
+}
---
+++ datastuctures/src/datastuctures/run.java
@@ -0,0 +1,15 @@
+package datastuctures;
+
+public class run {
+
+	public static void main(String[] args) {
+		LinkedList link = new LinkedList();
+		link.insert(10);
+		link.insert(5);
+		link.insert(78);
+		link.insert(96);
+		link.show();
+
+	}
+
+}
